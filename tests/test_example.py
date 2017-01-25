b66d7aa0e67ad1204bb40b993fc14a2118a616fa
Antoine Gagné <gagnantoine@gmail.com>
2017-01-24 20:24:14 -0500

    Change test name

--- tests/test_example.py
+++ tests/test_example.py
@@ -7,10 +7,10 @@ def foo(x):
     return x
 
 
-def test_that_given_a_valid_x_then_it_returns_a_x():
+def test_that_given_a_valid_x_when_fooing_then_it_returns_a_x():
     assert foo(3) == 3
 
 
-def test_that_given_an_invalid_x_then_it_returns_an_exception():
+def test_that_given_an_invalid_x_when_fooing_then_it_returns_an_exception():
     with pytest.raises(Exception):
         foo(2)
