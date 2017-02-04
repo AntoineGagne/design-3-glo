4e46c0d8594bfb7e671455323043278082609e92
Antoine Gagné <gagnantoine@gmail.com>
2017-02-03 19:52:16 -0500

    Change name in test

--- tests/vision/test_contours.py
+++ tests/vision/test_contours.py
@@ -6,7 +6,7 @@ from tests.utils import list_files
 import design.vision.contours as contours
 
 SAMPLE_IMAGES = list_files('samples')
-CONTOURS_NUMBER_BY_IMAGES_WITH_GREEN_SQUARES = {
+CONTOURS_NUMBER_BY_ONBOARD_IMAGES_PATTERNS = {
     re.compile('boot.*'): 9,
     re.compile('arrow.*'): 7,
     re.compile('hat.*'): 9,
@@ -20,7 +20,7 @@ CONTOURS_NUMBER_BY_IMAGES_WITH_GREEN_SQUARES = {
 
 
 def test_that_given_an_image_with_green_square_when_find_green_square_coordinates_then_coordinates_are_found():
-    for pattern in CONTOURS_NUMBER_BY_IMAGES_WITH_GREEN_SQUARES.keys():
+    for pattern in CONTOURS_NUMBER_BY_ONBOARD_IMAGES_PATTERNS.keys():
         for image in filter(pattern.search, SAMPLE_IMAGES):
             opened_image = cv2.imread(image, 1)
             green_square_contours = contours.find_green_square_coordinates(opened_image)
