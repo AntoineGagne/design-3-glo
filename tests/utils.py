d987f83a36ed8753073d2106a5cd2086e46cbede
Antoine Gagné <gagnantoine@gmail.com>
2017-02-10 20:19:59 -0500

    Refactor `compare_objects` function

--- tests/utils.py
+++ tests/utils.py
@@ -6,7 +6,5 @@ def compare_objects(object_1, object_2):
     :raises AssertionError: If the two object are not equal or not of the same
                             types
     """
-    assert isinstance(object_1, object_2.__class__)
-
-    for attribute in object_1.__dict__.keys():
-        assert object_1.__dict__.get(attribute) == object_2.__dict__.get(attribute)
+    assert (isinstance(object_1, object_2.__class__) and
+            object_1.__dict__ == object_2.__dict__)
