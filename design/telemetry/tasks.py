a29516e774590a91834b4844a07f02fc389eb03d
Antoine Gagné <gagnantoine@gmail.com>
2017-02-23 09:00:41 -0500

    Add a wrapper over the selector

---
+++ design/telemetry/tasks.py
@@ -0,0 +1,60 @@
+"""Contains all the classes and functions related to the tasks handling of the
+   telemetry package.
+"""
+
+
+import queue
+import threading
+
+from .selectors import Selector
+
+
+class TaskHandler:
+    """Handle accesses to the tasks queues."""
+
+    def __init__(self,
+                 selector: Selector,
+                 consumed: queue.Queue,
+                 produced: queue.Queue):
+        """Initialize the `TaskHandler`.
+
+        :param selector: The selector that will be used to fetch/send the
+                         packets to the other connected host
+        :type selector: :mod:`design.telemetry.selectors.Selector`
+        :param consumed: The tasks to be sent over the network
+        :type consumed: :mod:`queue.Queue`
+        :param produced: The tasks to be handled by the AI
+        :type produced: :mod:`queue.Queue`
+        """
+        self.consumed = consumed
+        self.produced = produced
+
+        self.selector = selector
+        self.selector_thread = threading.Thread(target=self.selector.run,
+                                                daemon=True)
+        self.selector_thread.start()
+
+    def put_task(self, packet):
+        """Add a task to be sent.
+
+        :param packet: The packet to be sent
+        :type packet: :mod:`design.telemetry.packets.Packet`
+        """
+        try:
+            self.consumed.put_nowait(packet)
+        except queue.Full:
+            pass
+
+    def fetch_task(self):
+        """Fetch a task from the received ones.
+
+        :returns: A packet that was received from the network
+        :rtype: :mod:`design.telemetry.packets.Packet`
+        """
+        data = None
+        try:
+            data = self.produced.get_nowait()
+        except queue.Empty:
+            pass
+
+        return data
