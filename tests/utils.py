385df1c94c894e376bb3bf97ff9dc69a1896e370
Antoine Gagné <AntoineGagne@users.noreply.github.com>
2017-02-26 12:43:07 -0500

    Feature/telemetry (#14)
    * Remove data directory
    
    * Add a draft of packets
    
    * Fix test by adding a function to compare objects
    
    * Refactor `compare_objects` function
    
    * Add TCP socket wrapper
    
    * Add some comments to the `sockets` module
    
    * Put some magic values inside a module
    
    * Fix typos
    
    * Add an alias for the `receive` method
    
    * Add a test that check if the socket can connect
    
    * Add a test to check that the socket are able to connect to one another
    
    * Add another test
    
    Some way to reuse address and port should be found. Otherwise, it might
    crash randomly
    
    * Remove `poll` method from the socket
    
    * Return `None` when `receive` could not retrieve any data
    
    * Fix type error in type hinting
    
    * Add a class to select sockets according to some events
    
    * Add factories
    
    * Handle connection returns
    
    * Add bytearray conversion
    
    * Change sockets to use ZeroMQ instead of standard library
    
    * Add compression along with pickling
    
    * Change `Selector` class to use ZeroMQ sockets
    
    * Remove `sockets` module
    
    * Add keyword arguments to be able to specify serialize/deserialize
    functions
    
    * Add a constant to specify the polling timeout
    
    * Remove useless print
    
    * Add `pyzmq` to the requirements
    
    * Add a wrapper over the selector
    
    * Make the attributes of the TaskHandler private
    
    * Add specification to the `fetch_task` method of the `TaskHandler` class
    
    * Rename tasks for commands
    
    * Fix docstring
    
    * Fix docstring

--- tests/utils.py
+++ tests/utils.py
@@ -2,6 +2,18 @@ import os
 import math
 
 
+def compare_objects(object_1, object_2):
+    """Compare two object for equality.
+
+    :param object_1: The first object
+    :param object_2: The second object
+    :raises AssertionError: If the two object are not equal or not of the same
+                            types
+    """
+    assert (isinstance(object_1, object_2.__class__) and
+            object_1.__dict__ == object_2.__dict__)
+
+
 def list_files(folder):
     """List all the files recursively within a given folder.
 
