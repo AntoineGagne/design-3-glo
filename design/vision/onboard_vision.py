67b4c83a5c4b354d7bf8bbab43bd52a7ea1b5d26
Antoine Gagné <gagnantoine@gmail.com>
2017-03-15 14:49:09 -0400

    Change the wrapper class

--- design/vision/onboard_vision.py
+++ design/vision/onboard_vision.py
@@ -1,8 +1,13 @@
 """Contains code related to the detection of figure's vertices with a camera."""
 
+import numpy as np
+
+from itertools import chain
+from typing import List
+
 from design.vision.camera import CameraInMemory
-from design.vision.transformations import (Figure,
-                                           RotateTransformation,
+from design.vision.constants import PAINTING_DIMENSION_RATIO
+from design.vision.transformations import (RotateTransformation,
                                            ScaleTransformation)
 
 
@@ -24,7 +29,9 @@ class OnboardVision:
         """Capture an image with the camera."""
         self.last_capture = self.camera.take_pictures(1)
 
-    def get_captured_vertices(self, zoom: float, orientation: float) -> Figure:
+    def get_captured_vertices(self,
+                              zoom: float,
+                              orientation: float) -> List[List[float]]:
         """Get the figure's vertices from the last captured image and apply the
            geometrical transformations on it.
 
@@ -32,8 +39,8 @@ class OnboardVision:
         :type zoom: float
         :param orientation: The orientation of the figure
         :type orientation: float
-        :returns: The detected and transformed figure
-        :rtype: :class:`design.vision.transformations.Figure`
+        :returns: The detected and transformed figure vertices' coordinates
+        :rtype: list<list<float>>
 
         :raises :class:`design.vision.exceptions.VerticesNotFound`:
             If no vertices could be found in the image
@@ -41,7 +48,18 @@ class OnboardVision:
             If the painting's frame could not be found in the image
         """
         figure = self.vertices_finder.find_vertices(self.last_capture)
-        return figure.apply_transformations(
+        transformed_figure = figure.apply_transformations(
             ScaleTransformation(zoom),
             RotateTransformation(orientation)
         )
+        vertices_coordinates = np.apply_along_axis(
+            lambda x: x * PAINTING_DIMENSION_RATIO,
+            2,
+            transformed_figure.coordinates
+        )
+        # OpenCV returns a matrix that looks like this:
+        # [[[a, b]],
+        #  ...
+        #  [[y, z]]]
+        # So we unpack it to remove the extra list from the result
+        return list(chain.from_iterable(vertices_coordinates))
